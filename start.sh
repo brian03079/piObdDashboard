b69cf5af0e69fe0e9552e8ebd102563f56167e00
brian03079 <brian.chan1@protonmail.com>
2021-03-13 15:57:34 -0500

    add start up file; modify script

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-xterm -e "node index.js; bash" &
+cd /home/pi/piObdDashboard
+/home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js &
 sleep 3
-xterm -e "python3 tempMonitor.py; bash" &
-xterm -e "python3 obdDash.py; bash" &
+python3 tempMonitor.py &
+python3 obdDash.py &
+chromium-browser --window-position=0,0 --kiosk
+unclutter -idle 1 &
