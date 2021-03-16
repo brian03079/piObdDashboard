38efc38318bbb337c9a8f4c795f54a06e16109ad
brian03079 <brian.chan1@protonmail.com>
2021-03-16 10:15:24 -0400

    add comments; update readme

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pi/piObdDashboard
-/home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js &
-sleep 3
+/home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js & #adjust path to node installation dir
+sleep 3 #give enough time for node server to start
 python3 tempMonitor.py &
 python3 obdDash.py &
 chromium-browser --window-position=0,0 --kiosk
