0960d7fdb30623f85031bfb5af4e4bbbffbcf9d1
brian03079 <brian.chan1@protonmail.com>
2021-03-18 07:59:15 -0400

    add chromium to start script

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/pi/piObdDashboard
+sleep 2
 /home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js &
-sleep 3
+sleep 4
 python3 tempMonitor.py &
 python3 obdDash.py &
 chromium-browser --window-position=0,0 --kiosk
