949ca79d287d172dc2975737fd45b69dc99466b1
brian03079 <brian.chan1@protonmail.com>
2021-07-21 16:29:01 -0400

    update restart and start scripts

--- restartApps.sh
+++ restartApps.sh
@@ -3,10 +3,11 @@
 sudo killall gpsd node python3 
 sudo gpsd /dev/serial0 -F /var/run/gpsd.sock #enable gpsd daemon for helping to parse gps data
 
+cd /home/pi/piObdDashboard #dir of all the apps to run
+
 #start the apps
-cd /home/pi/piObdDashboard
 /home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js & #need to manually specify installation dir if node was installed by nvm
-sleep 2 #give enough time for node server to start
+
 python3 tempMonitor.py &
 #python3 obdDash.py &
 python3 imuSensor.py &
--- startApps.sh
+++ startApps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sleep 2 #give enough time to go to desktop
 stty -F /dev/serial0 raw 9600 cs8 clocal -cstopb #ready the serial port for reading from Adafruit Ultimate GPS HAT
 sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
-#start the apps
+
 cd /home/pi/piObdDashboard
+
+#start the apps
 /home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js & #need to manually specify installation dir if node was installed by nvm
-sleep 2 #give enough time for node server to start
+
 python3 tempMonitor.py &
 #python3 obdDash.py &
 python3 imuSensor.py &
