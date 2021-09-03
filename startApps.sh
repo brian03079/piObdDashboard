7045b195ddcc1ad53e4710059f1f38d3693b16fc
Brian Chan <brian.chan1@protonmail.com>
2021-09-03 14:54:46 -0400

    fix to allow chrome to open on 2 different monitors at the same time; uncomment unclutter to hide mouse pointer

--- startApps.sh
+++ startApps.sh
@@ -5,13 +5,19 @@ sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
 cd /home/pi/piObdDashboard
 
 #start the apps
-/home/pi/.config/nvm/versions/node/v14.16.0/bin/node index.js & #need to manually specify installation dir if node was installed by nvm
+/home/pi/.nvm/versions/node/v16.8.0/bin/node index.js & #need to manually specify installation dir if node was installed by nvm
 
 python3 tempMonitor.py &
-#python3 obdDash.py &
+python3 obdDash.py &
 python3 imuSensor.py &
 python3 gpsModule.py &
 python3 airSensor.py &
 python3 obdMisc.py & #use python instead of python3 so it wont be terminated on restartApps.sh
-#chromium-browser --window-position=0,0 --kiosk
-#unclutter -idle 1 &
+
+cd /home/pi/piDashcam
+sudo python3 dashcam.py &
+
+chromium-browser --window-position=0,0    --kiosk --user-data-dir="/home/pi/Documents/Profiles/0" http://localhost:3000 &
+chromium-browser --window-position=600,0    --kiosk --user-data-dir="/home/pi/Documents/Profiles/1" http://localhost:3000/misc &
+
+unclutter -idle 1 &
