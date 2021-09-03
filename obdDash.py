eb3903250ac49fb6f3484cea256151e09f489803
Brian Chan <brian.chan1@protonmail.com>
2021-09-03 17:03:56 -0400

    reformat; use precise logging on initial connect

--- obdDash.py
+++ obdDash.py
@@ -3,37 +3,21 @@ import random
 import time
 import json
 import obd
+import obdUtils
 import time
 import re
-import plotly.graph_objs as go 
-from collections import deque 
-from obd import OBDStatus
 
 SENSOR_TYPE = 'OBD'
 ERROR = 'ERR'
+RETRY_INTERVAL = 1 #Delay in seconds when retrying to connect to node server
 
-
-delay = 0.3 #Delay in seconds before sending data
+delay = 0.3 #Delay in seconds before sending data. This can be decreased, but .3s seems to be the fastest
+            #that the pi can display on the web dashboard. Otherwise the web page updating begins lagging
 
 idleTime = 0.0 #Time the engine is idling in seconds
-maxIdleRpm = 1000 #rpm threshold that helps define if engine is idling
-maxThrottleActuatorIdle = 2.5
 
 currentDtcCodes = []
 dtcCodesChanged = False
-
-def createLogMessage(ex, logType, sensor):
-    now = datetime.datetime.now()
-    timestamp = "%d:%d:%d" % (now.hour, now.minute, now.second)
-            
-    return {
-        'logType': ERROR,
-        'sensor' : SENSOR_TYPE,
-        'exType' : type(ex).__name__,
-        'args' : ex.args,
-        'timeStamp': timestamp
-    }
-
 
 numTries = 1
 
@@ -79,11 +63,7 @@ def emitTelemetry():
             response = connection.query(runTimeCmd)
             runTime = str(response.value)
             
-            throttleActuatorCmd = obd.commands.THROTTLE_ACTUATOR # less than 2.5% at idle
-            response = connection.query(throttleActuatorCmd)
-            throttleActuator = response.value.magnitude
-            
-            if (speed < .1):
+            if (float(speed) < .1):
                 idleTime += delay
             
             data = {'speed': speed, 'rpm': rpm, 'throttle': throttle, 'runTime': runTime, 'idleTime': idleTime}
@@ -91,30 +71,30 @@ def emitTelemetry():
             
             emitDtcCodes()
             
-            time.sleep(.3)
+            time.sleep(RETRY_INTERVAL)
             
-        except Exception as ex: #logs any errors encountered during reading of gps. Also allows program to pick back up if node server connection is lost
-            errorLog = createLogMessage(ERROR, SENSOR_TYPE, type(ex).__name__, ex.args)
+        except Exception as ex: #logs any errors
+            errorLog = obdUtils.createLogMessage(ERROR, SENSOR_TYPE, type(ex).__name__, ex.args)
             print(errorLog)
             sio.emit('log', json.dumps(errorLog)) #will only work if exception is unrelated to node server connection
             continue
 
+connection = obd.OBD()
 
 while True: #loop until a connection is made with the server instead of immediately exiting
     try:
-        connection = obd.OBD()
         print("OBD connection established!")
         
         sio = socketio.Client()
         sio.connect('http://localhost:3000')
         emitTelemetry()
-        
-    except Exception as ex:
-        numTries += 1
-        print("Unable to connect to node server, retrying attempt {0}".format(numTries))
-        time.sleep(1)
-            
-        continue
+        break;
+    except Exception as ex: 
+            errorLog = obdUtils.createLogMessage(ERROR, SENSOR_TYPE, type(ex).__name__, ex.args)
+            print(errorLog)
+            sio.emit('log', json.dumps(errorLog)) #will only work if exception is unrelated to node server connection
+            sleep(RETRY_INTERVAL)
+            continue
         
 
 
